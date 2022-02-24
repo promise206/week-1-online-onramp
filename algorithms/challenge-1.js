49e6f6be7e998f4eb81b02671d06c76a183116a4
okpalaugo chukwuka promise <89579386+promise206@users.noreply.github.com>
2022-02-24 07:28:30 +0100

    changes challenge-1

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,15 @@
-function noOfElements(x, y, z) {}
+function noOfElements(x, y, z) {
+  let counter = 0;
+  let i = 0;
 
-module.exports = noOfElements
+  for (i = 0; i <= x.length; i++) {
+    if (x[i] >= y && x[i] <= z) {
+      counter = counter + 1;
+      if (i <= x.length - 1) {
+      }
+    }
+  }
+  return counter;
+}
+
+module.exports = noOfElements;
