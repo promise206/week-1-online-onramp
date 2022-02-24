d2e6ee05e7f27e6ec9cc919991e54eb040f6bde2
okpalaugo chukwuka promise <89579386+promise206@users.noreply.github.com>
2022-02-24 08:45:29 +0100

    changed challenge1-5

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,3 +1,15 @@
-function replaceDivisible(x, y) {}
+function replaceDivisible(x, y) {
+  let i = 0;
+  let index = 0;
+  for (i = 0; i <= x.length; i++) {
+    if (x[i] % y === 0) {
+      index = x.indexOf(x[i]);
+      if (index !== -1) {
+        x[index] = "isDivisible";
+      }
+    }
+  }
+  return x;
+}
 
-module.exports = replaceDivisible
+module.exports = replaceDivisible;
